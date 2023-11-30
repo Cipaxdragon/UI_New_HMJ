70166b8f578a5903f9ae584af37e3038f69724bf
cipaxdragon <asep@gmail.com>
2023-11-30 11:16:25 +0800

    footer baru

--- js/main.js
+++ js/main.js
@@ -21,3 +21,32 @@ document.addEventListener("DOMContentLoaded", function () {
         activeItem.style.opacity = 0.5;
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var animatedElements = document.querySelectorAll('.animated-element');
+
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function handleScroll() {
+        animatedElements.forEach(function(el) {
+            if (isElementInViewport(el)) {
+                el.classList.add('animated');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Initial check
+    handleScroll();
+});
+
